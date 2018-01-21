58ac529b9d06da8dc05a5fa6eae2a04e8be109b8
Vasia Patov <vasiapatov@gmail.com>
2018-01-21 18:55:29 -0500

    This is much harder than I thought. Gettin frustrated

--- 6.py
+++ 6.py
@@ -1,42 +1,199 @@
-plaintext = """Miusov, as a man man of breeding and deilcacy, could not but feel some inwrd qualms, when he reached the Father Superior's with Ivan: he felt ashamed of havin lost his temper. He felt that he ought to have disdaimed that despicable wretch, Fyodor Pavlovitch, too much to have been upset by him in Father Zossima's cell, and so to have forgotten himself. "Teh monks were not to blame, in any case," he reflceted, on the steps. "And if they're decent people here (and the Father Superior, I understand, is a nobleman) why not be friendly and courteous withthem? I won't argue, I'll fall in with everything, I'll win them by politness, and show them that I've nothing to do with that Aesop, thta buffoon, that Pierrot, and have merely been takken in over this affair, just as they have."
+import gmpy
+import array
+import base64
+from collections import Counter, defaultdict
 
-He determined to drop his litigation with the monastry, and relinguish his claims to the wood-cuting and fishery rihgts at once. He was the more ready to do this becuase the rights had becom much less valuable, and he had indeed the vaguest idea where the wood and river in quedtion were.
-
-These excellant intentions were strengthed when he enterd the Father Superior's diniing-room, though, stricttly speakin, it was not a dining-room, for the Father Superior had only two rooms alltogether; they were, however, much larger and more comfortable than Father Zossima's. But tehre was was no great luxury about the furnishng of these rooms eithar. The furniture was of mohogany, covered with leather, in the old-fashionned style of 1820 the floor was not even stained, but evreything was shining with cleanlyness, and there were many chioce flowers in the windows; the most sumptuous thing in the room at the moment was, of course, the beatifuly decorated table. The cloth was clean, the service shone; there were three kinds of well-baked bread, two bottles of wine, two of excellent mead, and a large glass jug of kvas -- both the latter made in the monastery, and famous in the neigborhood. There was no vodka. Rakitin related afterwards that there were five dishes: fish-suop made of sterlets, served with little fish paties; then boiled fish served in a spesial way; then salmon cutlets, ice pudding and compote, and finally, blanc-mange. Rakitin found out about all these good things, for he could not resist peeping into the kitchen, where he already had a footing. He had a footting everywhere, and got informaiton about everything. He was of an uneasy and envious temper. He was well aware of his own considerable abilities, and nervously exaggerated them in his self-conceit. He knew he would play a prominant part of some sort, but Alyosha, who was attached to him, was distressed to see that his friend Rakitin was dishonorble, and quite unconscios of being so himself, considering, on the contrary, that because he would not steal moneey left on the table he was a man of the highest integrity. Neither Alyosha nor anyone else could have infleunced him in that.
-
-Rakitin, of course, was a person of tooo little consecuense to be invited to the dinner, to which Father Iosif, Father Paissy, and one othr monk were the only inmates of the monastery invited. They were alraedy waiting when Miusov, Kalganov, and Ivan arrived. The other guest, Maximov, stood a little aside, waiting also. The Father Superior stepped into the middle of the room to receive his guests. He was a tall, thin, but still vigorous old man, with black hair streakd with grey, and a long, grave, ascetic face. He bowed to his guests in silence. But this time they approaced to receive his blessing. Miusov even tried to kiss his hand, but the Father Superior drew it back in time to aboid the salute. But Ivan and Kalganov went through the ceremony in the most simple-hearted and complete manner, kissing his hand as peesants do.
-
-"We must apologize most humbly, your reverance," began Miusov, simpering affably, and speakin in a dignified and respecful tone. "Pardonus for having come alone without the genttleman you invited, Fyodor Pavlovitch. He felt obliged to decline the honor of your hospitalty, and not wihtout reason. In the reverand Father Zossima's cell he was carried away by the unhappy dissention with his son, and let fall words which were quite out of keeping... in fact, quite unseamly... as" -- he glanced at the monks -- "your reverance is, no doubt, already aware. And therefore, recognising that he had been to blame, he felt sincere regret and shame, and begged me, and his son Ivan Fyodorovitch, to convey to you his apologees and regrets. In brief, he hopes and desires to make amends later. He asks your blessinq, and begs you to forget what has takn place."
-
-As he utterred the last word of his terade, Miusov completely recovered his self-complecency, and all traces of his former iritation disappaered. He fuly and sincerelly loved humanity again.
-
-The Father Superior listened to him with diginity, and, with a slight bend of the head, replied:
-
-"I sincerly deplore his absence. Perhaps at our table he might have learnt to like us, and we him. Pray be seated, gentlemen."
-
-He stood before the holly image, and began to say grace, aloud. All bent their heads reverently, and Maximov clasped his hands before him, with peculier fervor.
-
-It was at this moment that Fyodor Pavlovitch played his last prank. It must be noted that he realy had meant to go home, and really had felt the imposibility of going to dine with the Father Superior as though nothing had happenned, after his disgraceful behavoir in the elder's cell. Not that he was so very much ashamed of himself -- quite the contrary perhaps. But still he felt it would be unseemly to go to dinner. Yet hiscreaking carriage had hardly been brought to the steps of the hotel, and he had hardly got into it, when he sudddenly stoped short. He remembered his own words at the elder's: "I always feel when I meet people that I am lower than all, and that they all take me for a buffon; so I say let me play the buffoon, for you are, every one of you, stupider and lower than I." He longed to revenge himself on everone for his own unseemliness. He suddenly recalled how he had once in the past been asked, "Why do you hate so and so, so much?" And he had answered them, with his shaemless impudence, "I'll tell you. He has done me no harm. But I played him a dirty trick, and ever since I have hated him."
+plaintext = """Miusov, as a man man of breeding and deilcacy, could not but feel some
+inwrd qualms, when he reached the Father Superior's with Ivan: he felt ashamed of havin 
+lost his temper. He felt that he ought to have disdaimed that despicable wretch, Fyodor 
+Pavlovitch, too much to have been upset by him in Father Zossima's cell, and so to have 
+forgotten himself. "Teh monks were not to blame, in any case," he reflceted, on the steps.
+"And if they're decent people here (and the Father Superior, I understand, is a nobleman)
+why not be friendly and courteous withthem? I won't argue, I'll fall in with everything,
+I'll win them by politness, and show them that I've nothing to do with that Aesop, thta
+buffoon, that Pierrot, and have merely been takken in over this affair, just as they 
+have."He determined to drop his litigation with the monastry, and relinguish his claims 
+to the wood-cuting and fishery rihgts at once.
 """
+
 
 eng_freqs = {
 	'a':0.08167, 'b':0.01492, 'c':0.02782, 'd':0.04253, 'e':0.12702, 'f':0.02228, 'g':0.02015, 'h':0.06094, 
 	'i':0.06966, 'j':0.00153, 'k':0.00772, 'l':0.04025, 'm':0.02406, 'n':0.06749, 'o':0.07507, 'p':0.01929, 
 	'q':0.00095, 'r':0.05987, 's':0.06327, 't':0.09056, 'u':0.02758, 'v':0.00978, 'w':0.02360, 'x':0.00150, 
-	'y':0.01974, 'z':0.00074, ' ', 0.20
-	}
+	'y':0.01974, 'z':0.00074, ' ' : 0.20
+}
 
 def xor_encrypt(text,key):
 	output = []
 	key_index = 0
 	for i in range(0,len(text)):
-		output.append(ord(text[i]) ^ ord(key[key_index]))
+		if type(text) == str:
+			output.append(ord(text[i]) ^ ord(key[key_index]))
+		else:
+			output.append(text[i] ^ ord(key[key_index]))
+
 		key_index = (key_index + 1) % len(key)
 
 	return output
 
-key = 'OHMYGOD'
-output = xor_encrypt(plaintext,key)
-print(output)
-print(len(plaintext))
-print(len(output))
+
+def xor(data,key):
+	output = []
+	key_index = 0
+	for i in range(0,len(data)):
+		output.append(data[i] ^ key[key_index])
+		key_index = (key_index + 1) % len(key)
+
+	return array.array('B',output).tobytes()
+
+def hamming_distance(arr1,arr2):
+	_sum = 0
+	for b1,b2 in zip(arr1,arr2):
+		_sum += gmpy.popcount(b1 ^ b2)
+	return _sum
+
+assert(hamming_distance("wokka wokka!!!".encode(),"this is a test".encode()) == 37)
+
+
+# float calc_mrse(std::unordered_map<char,float> *freqs){
+#     int count_chars = 0;
+#     float sum;
+#     char c;
+
+#     sum = 0;
+#     for (std::unordered_map<char,float>::iterator it = freqs->begin(); it != freqs->end(); it++){
+#         c = (*it).first;
+#         if (eng_freqs.find(c) != eng_freqs.end()){
+#             sum += pow((*freqs)[c] - eng_freqs[c], 2);
+#             count_chars++;
+#         }
+#     }
+#     return (count_chars == 0) ? (std::numeric_limits<float>::max()) : (pow(sum / count_chars, 0.5));
+
+# }
+
+def calc_mrse(freqs):
+	c, count_chars, _sum = 0,0,0
+	for char in freqs:
+		pp = chr(char).lower()
+		if pp in eng_freqs:
+			_sum += (freqs[pp] - eng_freqs[pp])**2
+			count_chars += 1
+	return float("inf") if count_chars == 0 else (_sum / count_chars)**0.5
+
+def calc_freqs(data):
+	data = data.lower()
+	counts = Counter(data)
+	return {char:count/len(counts) for char,count in counts.items()}
+
+
+
+
+# void calc_freqs(float freqs[], byte bytes[], size_t strlen)
+# {
+#     int count_letters = 0;
+
+#     for (int i = 0; i < NUM_CHARS; i++)
+#         freqs[i] = 0;
+
+#     for (int i = 0; i < strlen; i++){
+#         if (bytes[i] >= 'a' && bytes[i] <= 'z'){
+#             freqs[bytes[i] - 'a']++;
+#             count_letters++;
+#         } else
+#         if (bytes[i] >= 'A' && bytes[i] <= 'Z'){
+#             freqs[bytes[i] - 'A']++;
+#             count_letters++;
+#         } else
+#         if (bytes[i] == ' '){
+#             freqs[NUM_CHARS-1]++;
+#         }
+#         else continue;
+
+#     }
+
+#     for (int i = 0; i < NUM_CHARS-1; i++){
+#         freqs[i] /= count_letters;
+#     }
+#     freqs[NUM_CHARS-1] /= strlen;
+# }
+
+f = open('6.txt','r')
+base64_input = ''.join([line.strip() for line in f.readlines()])
+data = base64.b64decode(base64_input)
+
+password = "OHMYGOD".encode()
+plaintext_bytes = plaintext.encode()
+data = array.array('B',xor(plaintext_bytes,password))
+
+
+
+min_keysize = 0
+min_hd = 99999
+hd = 0
+avg_hd = 0
+keysizes = []
+for keysize in range(2,40):
+	c = 0
+	for i in range(0,len(data) // keysize,2):
+		hd = hamming_distance(data[i*keysize:(i+1)*keysize],data[(i+1)*keysize:(i+2)*keysize])
+		avg_hd += hd
+		c += 1
+		# if (c == 3):
+			# break
+	avg_hd /= c
+	avg_hd /= keysize
+	keysizes.append((keysize,avg_hd))
+
+keysizes.sort(key=lambda x: x[1])
+print(keysizes)
+
+keysize = 7
+key = ''
+min_key = None
+min_mrse = float("inf")
+min_char = None
+for b1 in range(65,97):
+	print(b1)
+	for b2 in range(65,97):
+		key = (b1,b2)
+		freqs = defaultdict(int)
+		for i in range(0,len(data),keysize):
+			sample = xor(data[i:i+2],key)
+			print(key)
+			print(sample)
+			for char in sample:
+				freqs[char] += 1
+		for char in freqs:
+			freqs[char] /= len(freqs)
+
+		mrse = calc_mrse(freqs)
+		if (mrse < min_mrse):
+			min_mrse = mrse
+			min_key = key
+
+print(min_key)		
+
+
+
+# for i in range(0,len(data)//keysize,3):
+# 	min_mrse = float("inf")
+# 	min_char = '!!!DEATH!!!'
+# 	for b in range(ord('A'),ord('Z')):
+# 		freqs = calc_freqs(''.join([chr(char ^ b) for char in data[i:len(data):keysize]]))
+# 		freqs = calc_freqs(''.join([chr(char ^ b) for char in data[i:len(data):keysize]]))
+
+# 		mrse = calc_mrse(freqs)
+# 		if (mrse < min_mrse):
+# 			min_mrse = mrse
+# 			min_char = b
+
+# 		print(b, chr(b), mrse)
+# 	print()
+# 	key += chr(min_char)
+
+print(key)
+print(len(key))
+
+		
