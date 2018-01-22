e36ff4b4ea83d842b6aef6912e4314eacbd6efd1
Vasia Patov <vasiapatov@gmail.com>
2018-01-22 00:56:36 -0500

    Getting very close with 6. Have found the bugs. It got a bit messy, gonna clean up and try again when I have the time. Starting my first day as a full-time Research Scientist at Flashpoint tomorrowww :)

--- 6.py
+++ 6.py
@@ -3,18 +3,9 @@ import array
 import base64
 from collections import Counter, defaultdict
 
-plaintext = """Miusov, as a man man of breeding and deilcacy, could not but feel some
-inwrd qualms, when he reached the Father Superior's with Ivan: he felt ashamed of havin 
-lost his temper. He felt that he ought to have disdaimed that despicable wretch, Fyodor 
-Pavlovitch, too much to have been upset by him in Father Zossima's cell, and so to have 
-forgotten himself. "Teh monks were not to blame, in any case," he reflceted, on the steps.
-"And if they're decent people here (and the Father Superior, I understand, is a nobleman)
-why not be friendly and courteous withthem? I won't argue, I'll fall in with everything,
-I'll win them by politness, and show them that I've nothing to do with that Aesop, thta
-buffoon, that Pierrot, and have merely been takken in over this affair, just as they 
-have."He determined to drop his litigation with the monastry, and relinguish his claims 
-to the wood-cuting and fishery rihgts at once.
-"""
+f = open('last_question.txt','r')
+plaintext = f.read()
+f.close()
 
 
 eng_freqs = {
@@ -56,22 +47,6 @@ def hamming_distance(arr1,arr2):
 assert(hamming_distance("wokka wokka!!!".encode(),"this is a test".encode()) == 37)
 
 
-# float calc_mrse(std::unordered_map<char,float> *freqs){
-#     int count_chars = 0;
-#     float sum;
-#     char c;
-
-#     sum = 0;
-#     for (std::unordered_map<char,float>::iterator it = freqs->begin(); it != freqs->end(); it++){
-#         c = (*it).first;
-#         if (eng_freqs.find(c) != eng_freqs.end()){
-#             sum += pow((*freqs)[c] - eng_freqs[c], 2);
-#             count_chars++;
-#         }
-#     }
-#     return (count_chars == 0) ? (std::numeric_limits<float>::max()) : (pow(sum / count_chars, 0.5));
-
-# }
 
 def calc_mrse(freqs):
 	c, count_chars, _sum = 0,0,0
@@ -80,49 +55,22 @@ def calc_mrse(freqs):
 		if pp in eng_freqs:
 			_sum += (freqs[char] - eng_freqs[pp])**2
 			count_chars += 1
-	return float("inf") if count_chars == 0 else (_sum / count_chars)**0.5
+	return float("inf") if count_chars == 0 else (float(_sum) / count_chars)**0.5
 
 def calc_freqs(data):
 	data = data.lower()
 	counts = Counter(data)
-	return {char:count/len(counts) for char,count in counts.items()}
+	return {ord(char):count/len(data) for char,count in counts.items()}
 
 
 
-# void calc_freqs(float freqs[], byte bytes[], size_t strlen)
-# {
-#     int count_letters = 0;
 
-#     for (int i = 0; i < NUM_CHARS; i++)
-#         freqs[i] = 0;
-
-#     for (int i = 0; i < strlen; i++){
-#         if (bytes[i] >= 'a' && bytes[i] <= 'z'){
-#             freqs[bytes[i] - 'a']++;
-#             count_letters++;
-#         } else
-#         if (bytes[i] >= 'A' && bytes[i] <= 'Z'){
-#             freqs[bytes[i] - 'A']++;
-#             count_letters++;
-#         } else
-#         if (bytes[i] == ' '){
-#             freqs[NUM_CHARS-1]++;
-#         }
-#         else continue;
-
-#     }
-
-#     for (int i = 0; i < NUM_CHARS-1; i++){
-#         freqs[i] /= count_letters;
-#     }
-#     freqs[NUM_CHARS-1] /= strlen;
-# }
 
 f = open('6.txt','r')
 base64_input = ''.join([line.strip() for line in f.readlines()])
 data = base64.b64decode(base64_input)
 
-password = "OHMYGOD".encode()
+password = "ABCDEFG".encode()
 plaintext_bytes = plaintext.encode()
 data = array.array('B',xor(plaintext_bytes,password))
 
@@ -148,57 +96,61 @@ for keysize in range(2,40):
 keysizes.sort(key=lambda x: x[1])
 print(keysizes)
 
-keysize = 7
+keysize = len(password)
 key = ''
 min_key = None
 min_mrse = float("inf")
 max_mrse = 0
 max_key = None
 min_char = None
-for b1 in range(65,97):
-	print(b1)
-	for b2 in range(65,97):
-		key = (b1,b2)
-		freqs = defaultdict(int)
+# for b1 in range(65,91):
+# 	print(b1)
+# 	for b2 in range(65,91):
+# 		key = (b1,b2)
+# 		freqs = defaultdict(int)
+# 		count_sample = 0
+# 		for i in range(0,len(data),keysize):
 
-		for i in range(0,len(data),keysize):
-			sample = xor(data[i:i+2],key)
-			# print(sample, (i,i+2))
-			for char in sample:
-				freqs[char] += 1
-		for char in freqs:
-			freqs[char] /= len(freqs)
+# 			sample = xor(data[i:i+2],key)
+# 			if (key[0] == ord('Q') and key[1] == ord('W')):
+# 				print(sample)
+# 			# print(sample, (i,i+2))
+# 			for char in sample:
+# 				freqs[char] += 1
+# 			count_sample += len(sample)
+# 		for char in freqs:
+# 			freqs[char] /= count_sample
 
-		mrse = calc_mrse(freqs)
-		if (mrse < min_mrse):
-			min_mrse = mrse
-			min_key = key
-
-		if (mrse > max_mrse):
-			max_mrse = mrse
-			max_key = key
-
-		print("Key: ", chr(key[0]) + chr(key[1]))
-		print(mrse)
-
-print(chr(min_key[0]),chr(min_key[1]))		
-print(chr(max_key[0]),chr(max_key[1]))
-
-
-
-# for i in range(0,len(data)//keysize,3):
-# 	min_mrse = float("inf")
-# 	min_char = '!!!DEATH!!!'
-# 	for b in range(ord('A'),ord('Z')):
-# 		freqs = calc_freqs(''.join([chr(char ^ b) for char in data[i:len(data):keysize]]))
-# 		freqs = calc_freqs(''.join([chr(char ^ b) for char in data[i:len(data):keysize]]))
-
+# 		if (key[0] == ord('Q') and key[1] == ord('W')):
+# 			print(freqs)
+# 			print("SUM:", sum([val for key,val in freqs.items()]))
 # 		mrse = calc_mrse(freqs)
 # 		if (mrse < min_mrse):
 # 			min_mrse = mrse
-# 			min_char = b
+# 			min_key = key
 
-# 		print(b, chr(b), mrse)
-# 	print()
-# 	key += chr(min_char)
 
+
+# print(chr(min_key[0]),chr(min_key[1]))		
+
+
+
+for i in range(0,len(data)//keysize,3):
+	print(i)
+	min_mrse = float("inf")
+	min_char = '!!!DEATH!!!'
+	for b in range(ord('A'),ord('Z')):
+		freqs = calc_freqs(''.join([chr(char ^ b) for char in data[i:len(data):keysize]]))
+
+		# print(freqs)
+		mrse = calc_mrse(freqs)
+		if (mrse < min_mrse):
+			min_mrse = mrse
+			min_char = b
+
+		# print(b, chr(b), mrse)
+	# print()
+	key += chr(min_char)
+
+print(key)
+print(xor(data,key))
