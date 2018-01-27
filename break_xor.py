b118caba0ed43f929a5679cc4753e5c5ba41e26d
Vasia Patov <vpatov@flashpoint-intel.com>
2018-01-27 01:44:18 -0500

    I did it.

--- break_xor.py
+++ break_xor.py
@@ -1,6 +1,9 @@
 import os
 import array
 import base64
+import numpy as np
+import random
+import operator
 from collections import Counter, defaultdict
 # import threading
 import nltk
@@ -13,6 +16,9 @@ try:
 except:
 	gmpy_imported = False
 
+
+def gen_random_key(length):
+	return ''.join(random.sample(string.ascii_letters + string.digits,length))
 
 
 def xor_encrypt(text,key):
@@ -69,6 +75,7 @@ def calc_mrse_bytes(freqs):
 	c, count_chars, _sum = 0,0,0
 	for char in freqs:
 		pp = chr(char).lower()
+		# pp = chr(char)
 		if pp in nltk_eng_freqs:
 			_sum += (freqs[char] - nltk_eng_freqs[pp])**2
 			count_chars += 1
@@ -111,13 +118,75 @@ def try_keys(data,keysize,r1l,r1h,r2l,r2h, freqs):
 				min_mrse = mrse
 				min_key = key
 
-	print(''.join([chr(k) for k in min_key]))
-	print(min_key)
+	# print(''.join([chr(k) for k in min_key]))
+	# print(min_key)
 
-	print(keys)
+	# print(keys)
 	return keys
 
 
+def find_best_keysizes(data):
+	min_keysize = 0
+	min_hd = float("inf")
+	hd = 0
+	avg_hd = 0
+	keysizes = []
+	for keysize in range(2,40):
+		c = 0
+		for i in range(0,len(data) // keysize,2):
+			hd = hamming_distance(data[i*keysize:(i+1)*keysize],data[(i+1)*keysize:(i+2)*keysize])
+			avg_hd += hd
+			c += 1
+
+		avg_hd /= c
+		avg_hd /= keysize
+		keysizes.append((keysize,avg_hd))
+
+
+	keysizes.sort(key=operator.itemgetter(1))
+	for keysize in keysizes:
+		print(keysize)
+
+	return keysizes[0:1]
+	# print(keysizes)
+	# print([keyhd for keysize,keyhd in keysizes])
+
+
+	# diffs = []
+	# for i in range(0,len(keysizes) - 1):
+	# 	diffs.append(keysizes[i][1] / keysizes[i+1][1])
+
+	# stdev = np.std(diffs)
+	# cutoff= 1
+	# prev = diffs[0]
+	# for diff in diffs[1:]:
+	# 	if prev - diff > stdev:
+	# 		break
+	# 	cutoff += 1
+
+	# sorted_keysizes = [(keysize,keyhd) for keysize,keyhd in sorted(keysizes[:cutoff])]
+
+	# if the probable keysizes (before the cutoff) share the minimum keysize as a gcd, return 
+	# those keysizes sorted by keysize (ascending)
+
+	# if they do not share the minimum keysize as a gcd, then return those keysizes sorted 
+	# by mrse
+
+	# share_gcd = True
+	# gcd = min(sorted_keysizes)[0]
+	# for keysize in sorted_keysizes:
+	# 	if (keysize[0] % gcd != 0):
+	# 		share_gcd = False
+	# 		break
+	# if share_gcd:
+	# 	return sorted_keysizes
+	# else:
+	# 	return 
+
+	return sorted_keysizes
+
+def find_best_keysize(data):
+	return find_best_keysizes(data)[0][0]
 
 
 ## TODOe
@@ -130,12 +199,6 @@ def try_keys(data,keysize,r1l,r1h,r2l,r2h, freqs):
 # less confident neighbor and try to decrypt as a pair
 # 4) Implement parallelization
 
-eng_freqs = {
-	'a':0.08167, 'b':0.01492, 'c':0.02782, 'd':0.04253, 'e':0.12702, 'f':0.02228, 'g':0.02015, 'h':0.06094, 
-	'i':0.06966, 'j':0.00153, 'k':0.00772, 'l':0.04025, 'm':0.02406, 'n':0.06749, 'o':0.07507, 'p':0.01929, 
-	'q':0.00095, 'r':0.05987, 's':0.06327, 't':0.09056, 'u':0.02758, 'v':0.00978, 'w':0.02360, 'x':0.00150, 
-	'y':0.01974, 'z':0.00074, ' ' : 0.20
-}
 
 if (os.path.isfile('./nltk_eng_freqs.pickle')):
 	pickle_file = open('nltk_eng_freqs.pickle','rb')
@@ -148,6 +211,7 @@ else:
 	eng_corpus = nltk.Text(nltk.corpus.brown.words())
 	for word in eng_corpus:
 		for char in word.lower():
+		# for char in word:
 			nltk_eng_freqs[char] += 1
 		count_letters += len(word) + 1
 		nltk_eng_freqs[' '] += 1
@@ -160,57 +224,123 @@ else:
 	pickle.dump(nltk_eng_freqs,pickle_file)
 	pickle_file.close()
 
-print("NLTK_eng_freqs:", nltk_eng_freqs)
-print("brown mrse:", calc_mrse_chars(nltk_eng_freqs))
+
+def find_key_candidates1(data,keysize):
+	min_key = None
+	all_mrses = {}
+	for position in range(0,keysize):
+		min_mrse = float("inf")
+		mrses = {}
+		for key in range(0,255):
+			freqs = defaultdict(int)
+			for i in range(position,len(data),keysize):
+				decoded_byte = xor_byte(data[i],key)
+
+				# if its uppercase, lowercase it
+				if (decoded_byte >= 65 and decoded_byte <= 90):
+					decoded_byte += 32
+
+				freqs[decoded_byte] += 1
+			for b in freqs:
+				freqs[b] /= len(data)/float(keysize)
+
+			mrse = calc_mrse_bytes(freqs)
+			mrses[key] = mrse
+			# print(position,key,chr(key),mrse)
+			if mrse < min_mrse:
+				min_mrse = mrse
+				min_key = key
+		all_mrses[position] = mrses
+		# print(min_key, chr(min_key))
+
+	key_candidates = []
+	for position in all_mrses:
+		sorted_mrses = sorted(all_mrses[position].items(), key=lambda pair: pair[1])
+		key_candidates.append([pair[0] for pair in sorted_mrses[:2]])
+		print([(chr(pair[0]),pair[1]) for pair in sorted_mrses[:2]])
+
+	return key_candidates
+
+def find_key_candidates2(data,keysize):
+	key = array.array('B')
+	for position in range(0,keysize):
+		max_ratio = 0
+		max_key = None
+		for key_byte in range(0,255):
+			count_spaces = 0
+			for i in range(position,len(data),keysize):
+				decoded_byte = xor_byte(data[i],key_byte)
 
 
-f = open('last_question.txt','r')
-plaintext = f.read()
-f.close()
+				if decoded_byte == ord(' '):
+					count_spaces += 1
+			ratio = count_spaces / len(data) / keysize
 
-password = "ABfWRETe".encode()
-plaintext_bytes = plaintext.encode()
-data = array.array('B',xor(plaintext_bytes,password))
+			if ratio > max_ratio:
+				max_ratio = ratio
+				max_key = key_byte
 
-keysize = len(password)
-min_key = None
-min_mrse = float("inf")
-all_mrses = {}
-for position in range(0,keysize):
-	min_mrse = float("inf")
-	mrses = {}
-	for key in range(0,255):
-		freqs = defaultdict(int)
-		for i in range(position,len(data),keysize):
-			decoded_byte = xor_byte(data[i],key)
+		key.append(max_key)
 
-			# if its uppercase, lowercase it
-			if (decoded_byte >= 65 and decoded_byte <= 90):
-				decoded_byte += 32
-
-			freqs[decoded_byte] += 1
-		for b in freqs:
-			freqs[b] /= len(data)/float(keysize)
-
-		mrse = calc_mrse_bytes(freqs)
-		mrses[key] = mrse
-		# print(position,key,chr(key),mrse)
-		if mrse < min_mrse:
-			min_mrse = mrse
-			min_key = key
-	all_mrses[position] = mrses
-
-	print(min_key, chr(min_key))
-	print('\n\n')
-
-for position in all_mrses:
-	sorted_mrses = sorted(all_mrses[position].items(), key=lambda pair: pair[1])
-	print([chr(pair[0]) for pair in sorted_mrses[:3]])
+	return key
 
 
-# f = open('6.txt','r')
-# base64_input = ''.join([line.strip() for line in f.readlines()])
-# data = base64.b64decode(base64_input)
+
+def crack(data, key_candidates):
+	pass
+
+def run():
+
+	# f = open('last_question.txt','r')
+	# plaintext = f.read()
+	# f.close()
+
+	# password = "abcdefghi".encode()
+	# plaintext_bytes = plaintext.encode()
+	# data = array.array('B',xor(plaintext_bytes,password))
+
+	f = open('6.txt','r')
+	base64_input = ''.join([line.strip() for line in f.readlines()])
+	data = base64.b64decode(base64_input)
+
+	
+
+
+	keysizes = find_best_keysizes(data)
+	print(keysizes)
+	keysize = keysizes[0][0]
+
+
+	# key_candidates = find_key_candidates1(data,keysize)
+	# for position in key_candidates:
+	# 	print([chr(char) for char in position])
+	
+	# answer = array.array('B')
+	# for position in key_candidates:
+	# 	answer.append(position[0])
+
+	# print(answer.tobytes())
+	# print(password)
+
+
+	answer2 = find_key_candidates2(data,keysize)
+	print(answer2.tobytes())
+	print(xor(data,answer2))
+
+
+
+
+if __name__ == '__main__':
+	run()
+
+
+
+
+
+
+
+
+
 
 
 
@@ -220,25 +350,6 @@ for position in all_mrses:
 
 
 
-min_keysize = 0
-min_hd = 99999
-hd = 0
-avg_hd = 0
-keysizes = []
-for keysize in range(2,40):
-	c = 0
-	for i in range(0,len(data) // keysize,2):
-		hd = hamming_distance(data[i*keysize:(i+1)*keysize],data[(i+1)*keysize:(i+2)*keysize])
-		avg_hd += hd
-		c += 1
-		# if (c == 3):
-			# break
-	avg_hd /= c
-	avg_hd /= keysize
-	keysizes.append((keysize,avg_hd))
-
-keysizes.sort(key=lambda x: x[1])
-print(keysizes)
 
 
 
